82e247b4b9a8ae651c2013a3d3e16304eab7caee
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-01-01 23:00:42 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed1879be3c9fbf3a96550718704b0ae9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9e1ee1c-65bd-4368-b7c6-729b00cc3a85'
+};
